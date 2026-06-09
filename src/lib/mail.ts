a8b4251683f22deff7a1b6342bcb2e28e9083ca9
Hellen <hellen.rubio@udea.edu.co>
2026-06-09 00:43:19 -0500

    adición configuración de SonarCloud

--- src/lib/mail.ts
+++ src/lib/mail.ts
@@ -5,16 +5,9 @@
 import { Resend } from "resend";
 import ResetPasswordEmail from "@/emails/ResetPasswordEmail";
 
-function getResendClient() {
-    const apiKey = process.env.RESEND_API_KEY;
-    if (!apiKey) {
-        throw new Error("RESEND_API_KEY must be defined");
-    }
-    return new Resend(apiKey);
-}
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function sendResetEmail(to: string, name: string, link: string) {
-    const resend = getResendClient();
     // Nos aseguramos que el "to" esté en formato válido
     // const formattedTo = `${name} <${to}>`;
     try {
